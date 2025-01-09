12061787f125d9a2a7572f5ba418e678b1977417
PolinaRomanchuk <polinar_99@mail.ru>
2025-01-09 19:27:42 +0300

    feat: add game field

--- simon-says/script.js
+++ simon-says/script.js
@@ -1,4 +1,5 @@
 let body = document.body;
+let level = "Easy";
 
 function createRuleWindow() {
   let ruleContainer = createElement("div", "rule-window-container", "");
@@ -10,12 +11,34 @@ function createRuleWindow() {
   body.append(ruleContainer);
 }
 
+function createGame() {
+  let gameContainer = createElement("div", "game-window-container", "");
+  let selectedLevel = createElement("div", "selected-level", level);
+  let gameField = createGameField(createAlphabetArray());
+  
+  let userField = createElement("input", "user-input", "");
+
+  let manageContainer = createElement("div", "manage-container");
+  let stage = createElement("div", "user-stage", "1/5");
+  let gameBtn = createElement("button", "repeat-btn", "Repeat");
+
+  manageContainer.append(stage);
+  manageContainer.append(gameBtn);
+
+  gameContainer.append(selectedLevel);
+  gameContainer.append(gameField);
+  gameContainer.append(userField);
+  gameContainer.append(manageContainer);
+
+  body.append(gameContainer);
+}
+
 function createElement(tagName, className, context) {
   let element = document.createElement(tagName);
   if (className) {
     element.classList.add(className);
   }
-  if (context) {
+  if (context !== undefined && context !== null) {
     element.textContent = context;
   }
   return element;
@@ -72,4 +95,31 @@ function createLevelBox() {
   return levelContainer;
 }
 
+function createGameField(arr) {
+  let gameField = createElement("div", "game-field", "");
+
+  arr.map((el) => {
+    let button = createElement("button", "element-game", el);
+    gameField.append(button);
+  });
+  return gameField;
+}
+
+function createNumberArray() {
+  let numberArray = [];
+  for (let i = 0; i <= 9; i++) {
+    numberArray.push(i);
+  }
+  return numberArray;
+}
+
+function createAlphabetArray() {
+  let alphabetArray = [];
+  for (let i = 97; i <= 122; i++) {
+    alphabetArray.push(String.fromCharCode(i));
+  }
+  return alphabetArray;
+}
+
 createRuleWindow();
+createGame();

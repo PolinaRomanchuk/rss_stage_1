5eb302652e84cd699f35814ac1d10f0196fbb80c
PolinaRomanchuk <polinar_99@mail.ru>
2025-03-08 17:30:05 +0300

    chore: add configurations

---
+++ decision-making-tool/lint-staged.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '**/*.{ts,js}': ['eslint --fix', 'prettier --write'],
+  '**/*.css': ['stylelint --fix', 'prettier --write'],
+};
---
+++ decision-making-tool/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  }
+};

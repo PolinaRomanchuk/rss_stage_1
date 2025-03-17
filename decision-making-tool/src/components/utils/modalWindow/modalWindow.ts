b016d79aa32221bb303620daacacc1050daf7f7f
PolinaRomanchuk <polinar_99@mail.ru>
2025-03-17 18:53:59 +0300

    feat: add new close function and overflow to modal window

--- decision-making-tool/src/components/utils/modalWindow/modalWindow.ts
+++ decision-making-tool/src/components/utils/modalWindow/modalWindow.ts
@@ -1,9 +1,10 @@
 import BaseView from '../../view/baseView';
-import '../modalWindow/modalWindow.css'
+import '../modalWindow/modalWindow.css';
 
 class ModalWindow extends BaseView {
   private contentContainer: BaseView;
   private closeButton: BaseView;
+  private isActive: boolean = false;
 
   constructor() {
     super({
@@ -24,6 +25,16 @@ class ModalWindow extends BaseView {
 
     modalContainer.appendChildren([this.closeButton, this.contentContainer]);
     this.append(modalContainer);
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') this.close();
+    });
+
+    this.getBaseElement().addEventListener('click', (event) => {
+      if (event.target === this.getBaseElement()) {
+        this.close();
+      }
+    });
   }
 
   public setContent(content: HTMLElement): void {
@@ -32,10 +43,22 @@ class ModalWindow extends BaseView {
 
   public open(): void {
     document.body.append(this.getBaseElement());
+    this.isActive = true;
+    this.setOverflow();
   }
 
   public close(): void {
     this.removeElement();
+    this.isActive = false;
+    this.setOverflow();
+  }
+
+  private setOverflow() {
+    if (this.isActive) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
   }
 }
 
